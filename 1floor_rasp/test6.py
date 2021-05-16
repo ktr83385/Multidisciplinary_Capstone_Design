5da9cb5b1095da750df9ebac997c9c33fed11c63
KimYutan <70746658+yutan0565@users.noreply.github.com>
2021-05-16 15:28:26 +0900

    co_co2 - temp,hum

--- 1floor_rasp/test6.py
+++ 1floor_rasp/test6.py
@@ -21,6 +21,77 @@ mqttc.loop_start()
 #MQTT data
 data1 = " "
 
+#이산화 탄소 400이하 값 처리
+def CO2_ppm_min(co2_ppm):
+  if co2_ppm < 400:
+    co2_ppm = 400
+  return co2_ppm
+
+#일산화 탄소 음수 처리
+def CO_ppm_positive(co_ppm):
+  if co_ppm < 0:
+    co_ppm = 0
+  return co_ppm
+
+#온도에 따른 co값 적용
+
+def CO_temp_ppm(temp, co_ppm):
+  if temp <= -16:
+    rate = 1*( temp +16 ) + 73
+  elif temp <= -4:
+    rate = 0.916667*( temp +4 ) + 84
+  elif temp <= 8:
+    rate = 0.75*( temp - 8 ) + 93
+  elif temp <= 20:
+    rate = 0.583333*( temp - 20 ) + 100
+  elif temp <= 36:
+    rate = 0.3125*( temp - 36 ) + 105
+  else:
+    rate = 0.214286*( temp - 50 ) + 108
+  return co_ppm*rate
+
+#온도에 따른 co2값 적용
+
+def CO2_temp_v(temp, co2_v):
+  if temp <= 0:
+    co2_v = 0.1*( temp ) + 338.2
+  elif temp <= 10:
+    co2_v = 0.18*( temp - 10 ) + 340
+  elif temp <= 20:
+    co2_v = 0.14*( temp - 20 ) + 341.4
+  elif temp <= 30:
+    co2_v = 0.16*( temp - 30 ) + 343
+  else:
+    co2_v = 0.14*( temp - 50 ) + 345.8
+  return co2_v
+
+#습도에 따른 co2값 적용
+
+def CO2_hum_v(hum, co2_v):
+  if hum <= 40:
+    co2_v = -0.025*( hum - 40 ) + 350.3
+  elif hum <= 65.5:
+    co2_v = -0.01961*( hum - 65.5 ) + 349.8
+  else:
+    co2_v = -0.12308*( hum - 85 ) + 347.4
+  return co2_v
+
+#전압에 따른 풍속 출력
+
+def wind_speed( vdc):
+  speed = 0
+  if vdc <= 0.5:
+    speed = 0
+  elif vdc <= 0.7:
+    speed = 0.266667(vdc - 0.7) + 0.75
+  elif vdc <= 0.7:
+    speed = 0.546667(vdc - 1.11) + 1.5
+  elif vdc <= 1.58:
+    speed = 0.546667(vdc - 1.58) + 2.25
+  else:
+    speed = 0.56(vdc - 2) + 3
+  return speed
+
 #MQTT connect
 def on_connect(client, userdata, flags, rc):
 	print("Connected with result code " + str(rc))
@@ -48,11 +119,17 @@ def on_message(client, userdata, msg):
 		CO_vref_value = readadc(CO_vref_channel)
 		CO2_value = readadc(CO2_channel)
 		
-		CO_ppm = CO_1_M*((3*CO_vgas_value/1024)-(3*CO_vref_value/1024))
+		#CO_ppm = CO_1_M*((3*CO_vgas_value/1024)-(3*CO_vref_value/1024))
+        CO_ppm = CO_ppm_positive(CO_temp_ppm(temp, CO_1_M * ((3 * CO_vgas_value / 1024) - (3 * CO_vref_value / 1024))))
+
 		
 		CO2_v = 3.3*CO2_value/1024
 		CO2_v_di = CO2_v / 8.5
-		CO2_ppm = pow(10, ((CO2_v_di - 0.079) / CO2_slope + 3.698))
+
+
+        CO2_v_di = CO2_hum_v(CO2_temp_v(temp,CO2_v_di))
+
+		CO2_ppm = pow(10, (humi, (CO2_v_di - 0.079) / CO2_slope + 3.698))
 		
 		_point1 = Point("sangrokwon").tag("location", "3_floor").field("PM1.0", PM1_0)
 		_point2 = Point("sangrokwon").tag("location", "3_floor").field("PM2.5", PM2_5)
